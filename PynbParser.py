2522af3db0773bc38edcd2d07b605f41b49b23ee
Bryan Kwong <bfkwong@calpoly.edu>
2020-07-05 18:38:04 -0700

    Update PynbParser.py

--- PynbParser.py
+++ PynbParser.py
@@ -13,163 +13,87 @@ OUTPUT_IMG_CNT = 0
 OPERATING_PATH = ""
 CHAPTER_TOP = True
 
-graphicsRE = r'(?P<pre>[\s\S]*)\\includegraphics{(?P<pngSrc>[\s\S]*?)}(?P<post>[\s\S]*)'
-def parseIncludeGraphics(string):
+sectionRE = r'\\section{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
+subsectionRE = r'\\subsection{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
+subsubsectionRE = r'\\subsubsection{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
+graphicsRE = r'\\includegraphics{(?P<pngSrc>[\s\S]*?)}(?P<post>[\s\S]*)'
+quotationRE = r'\\begin{displayquote}\n(?P<title>[\s\S]*?)\\end{displayquote}(?P<post>[\s\S]*)'
+mathJaxAlignRE = r'\$\$ *\n* *\\begin{align}(?P<title>[\s\S]*?) *\n* *\\end{align}\$\$(?P<post>[\s\S]*)'
+tagRE = r'\\tag{[\s\S]*?}(?P<post>[\s\S]*)'
+hrefTagsRE = r'\\href{(?P<link>[\s\S]*?)}{(?P<text>[\s\S]*?)}(?P<post>[\s\S]*)'
+
+def postParserHelper(string):
     global OUTPUT_IMG_CNT
-    res = re.search(graphicsRE, string)
+    if string == "":
+        return "", ""
+    if CHAPTER_TOP:
+        chapterMatch = re.match(sectionRE, string)
+        if chapterMatch:
+            titleSplit = chapterMatch["title"].split(" ")
+            chLabel = titleSplit[0]
+            chTag = " ".join(titleSplit[1:])
+            body = "\\chapter{%s}\\label{%s}" % (chTag, chLabel)
+            return body, chapterMatch["post"]
 
-    if res == None:
-        return string
+        sectionMatch = re.match(subsectionRE, string)
+        if sectionMatch:
+            titleSplit = sectionMatch["title"].split(" ")
+            secLabel = titleSplit[0]
+            secTag = " ".join(titleSplit[1:])
+            body = "\\section{%s}\\label{%s}" % (secTag, secLabel)
+            return body, sectionMatch["post"]
 
-    res = res.groupdict()
+        subsectionMatch = re.match(subsubsectionRE, string)
+        if subsectionMatch:
+            titleSplit = subsectionMatch["title"].split(" ")
+            subsecLabel = titleSplit[0]
+            subsecTag = " ".join(titleSplit[1:])
+            body = "\\subsection{%s}\\label{%s}" % (subsecTag, subsecLabel)
+            return body, subsectionMatch["post"]
 
-    preRE = parseIncludeGraphics(res["pre"])
-    postRE = parseIncludeGraphics(res["post"])
+    graphicsMatch = re.match(graphicsRE, string)
+    if graphicsMatch:
+        if graphicsMatch["pngSrc"][0:4] == "http":
+            urllib.request.urlretrieve(graphicsMatch["pngSrc"], "./LatexBook/image/" + str(OUTPUT_IMG_CNT) + ".png")
+            body = "\\begin{figure}[h!]\\centering\\includegraphics[width=0.45\\paperwidth]{%s}\\end{figure}\n" % ("image/" + str(OUTPUT_IMG_CNT) + ".png")
+            OUTPUT_IMG_CNT += 1
+        else:
+            with open(OPERATING_PATH + graphicsMatch["pngSrc"], "rb") as f:
+                with open("./LatexBook/image/" + graphicsMatch["pngSrc"] + ".png", "wb") as f2:
+                    f2.write(f.read())
+            body = "\\begin{figure}[h!]\\centering\\includegraphics[width=0.45\\paperwidth]{%s}\\end{figure}\n" % ("image/" + graphicsMatch["pngSrc"] + ".png")
+        return body, graphicsMatch["post"]
 
-    if res["pngSrc"][0:4] == "http":
-        urllib.request.urlretrieve(res["pngSrc"], "./LatexBook/image/" + str(OUTPUT_IMG_CNT) + ".png")
-        body = "\\begin{figure}[h!]\\centering\\includegraphics[width=0.45\\paperwidth]{%s}\\end{figure}\n" % ("image/" + str(OUTPUT_IMG_CNT) + ".png")
-        OUTPUT_IMG_CNT += 1
-    else:
-        with open(OPERATING_PATH + res["pngSrc"], "rb") as f:
-            with open("./LatexBook/image/" + res["pngSrc"] + ".png", "wb") as f2:
-                f2.write(f.read())
-        body = "\\begin{figure}[h!]\\centering\\includegraphics[width=0.45\\paperwidth]{%s}\\end{figure}\n" % ("image/" + res["pngSrc"] + ".png")
+    quotationMatch = re.match(quotationRE, string)
+    if quotationMatch:
+        body = "\\begin{displayquote}" + quotationMatch["title"] + "\\end{displayquote}"
+        return body, quotationMatch["post"]
 
-    return preRE + body + postRE
+    mathJaxAlignMatch = re.match(mathJaxAlignRE, string)
+    if mathJaxAlignMatch:
+        body = "\\begin{align}" + mathJaxAlignMatch["title"] + "\\end{align}"
+        return body, mathJaxAlignMatch["post"]
 
-sectionRE = r'(?P<pre>[\s\S]*)\\section{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
-def parseSection(string):
-    global OUTPUT_IMG_CNT
-    res = re.search(sectionRE, string)
+    tagMatch = re.match(tagRE, string)
+    if tagMatch:
+        return "", tagMatch["post"]
 
-    if res == None:
-        return string
+    hrefMatch = re.match(hrefTagsRE, string)
+    if hrefMatch:
+        body = hrefMatch["text"] + "\\footnote{" + hrefMatch["link"] + "}"
+        return body, hrefMatch["post"]
 
-    res = res.groupdict()
+    return string[0], string[1:]
 
-    preRE = parseSection(res["pre"])
-    postRE = parseSection(res["post"])
+def postParser(string):
+    parsed = []
+    toBeParsed = string
+    while toBeParsed != "":
+        body, post = postParserHelper(toBeParsed)
+        parsed.append(body)
+        toBeParsed = post
+    return "".join(parsed)
 
-    titleSplit = res["title"].split(" ")
-    chLabel = titleSplit[0]
-    chTag = " ".join(titleSplit[1:])
-    body = "\\chapter{%s}\\label{%s}" % (chTag, chLabel)
-
-    return preRE + body + postRE
-
-subsectionRE = r'(?P<pre>[\s\S]*)\\subsection{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
-def parseSubSection(string):
-    global OUTPUT_IMG_CNT
-    res = re.search(subsectionRE, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseSubSection(res["pre"])
-    postRE = parseSubSection(res["post"])
-
-    titleSplit = res["title"].split(" ")
-    secLabel = titleSplit[0]
-    secTag = " ".join(titleSplit[1:])
-    body = "\\section{%s}\\label{%s}" % (secTag, secLabel)
-
-    return preRE + body + postRE
-
-subsubsectionRE = r'(?P<pre>[\s\S]*)\\subsubsection{(?P<title>[\s\S]*?)}(?P<post>[\s\S]*)'
-def parseSubSubSection(string):
-    global OUTPUT_IMG_CNT
-    res = re.search(subsubsectionRE, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseSubSubSection(res["pre"])
-    postRE = parseSubSubSection(res["post"])
-
-    titleSplit = res["title"].split(" ")
-    subsecLabel = titleSplit[0]
-    subsecTag = " ".join(titleSplit[1:])
-    body = "\\subsection{%s}\\label{%s}" % (subsecTag, subsecLabel)
-
-    return preRE + body + postRE
-
-quotationRE = r'(?P<pre>[\s\S]*)\\begin{displayquote}\n(?P<title>[\s\S]*?)\\end{displayquote}(?P<post>[\s\S]*)'
-def parseQuotation(string):
-    res = re.search(quotationRE, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseQuotation(res["pre"])
-    postRE = parseQuotation(res["post"])
-    body = "\\begin{displayquote}" + res["title"] + "\\end{displayquote}"
-
-    return preRE + body + postRE
-
-mathJaxAlignRE = r'(?P<pre>[\s\S]*)\$\$ *\n* *\\begin{align}(?P<title>[\s\S]*?) *\n* *\\end{align}\$\$(?P<post>[\s\S]*)'
-def parseMathJaxTags(string):
-    res = re.search(mathJaxAlignRE, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseMathJaxTags(res["pre"])
-    postRE = parseMathJaxTags(res["post"])
-    body = "\\begin{align}" + res["title"] + "\\end{align}"
-
-    return preRE + body + postRE
-
-tagRE = r'(?P<pre>[\s\S]*)\\tag{[\s\S]*?}(?P<post>[\s\S]*)'
-def parseRemoveTag(string):
-    res = re.search(tagRE, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseRemoveTag(res["pre"])
-    postRE = parseRemoveTag(res["post"])
-
-    return preRE + postRE
-
-hrefTags = r'(?P<pre>[\s\S]*)\\href{(?P<link>[\s\S]*?)}{(?P<text>[\s\S]*?)}(?P<post>[\s\S]*)'
-def parseFootNoteTag(string):
-    res = re.search(hrefTags, string)
-
-    if res == None:
-        return string
-
-    res = res.groupdict()
-
-    preRE = parseFootNoteTag(res["pre"])
-    postRE = parseFootNoteTag(res["post"])
-    body = res["text"] + "\\footnote{" + res["link"] + "}"
-
-    return preRE + body + postRE
-
-
-def postParser(string, chapterTop):
-    parsed = string
-    if chapterTop:
-        parsed = parseSection(parsed)
-        parsed = parseSubSection(parsed)
-        parsed = parseSubSubSection(parsed)
-    parsed = parseIncludeGraphics(parsed)
-    parsed = parseQuotation(parsed)
-    parsed = parseMathJaxTags(parsed)
-    parsed = parseRemoveTag(parsed)
-    parsed = parseFootNoteTag(parsed)
-    return parsed
 
 def openPynb(filename):
     with open(filename, mode= "r", encoding= "utf-8") as f:
@@ -236,7 +160,7 @@ def pynbParser(indir, outfile, indexWords):
                     rendered = mistletoe.markdown("".join(cell["source"]), LaTeXRenderer)
                     latexRE = r'[\s\S]*\\begin{document}(?P<body>[\s\S]*)\\end{document}'
                     s = re.match(latexRE, rendered).groupdict()
-                    s = postParser(s["body"], CHAPTER_TOP)
+                    s = postParser(s["body"])
                     for word in indexWords:
                         s = s.replace(word, "\\index{" + word + "}")
                     parsedCells.append(s)
@@ -300,7 +224,7 @@ def createLatexBook(configfilename):
         output = pynbParser(bookdir + "/" + folder, "out.tex", indexWords)
         outputs.append([bookdir + "/" + folder, output])
 
-        main_written.append("\include{" + bookdir + "/" + folder + "/out" + "}")
+        main_written.append("\include{LatexBook/" + bookdir + "/" + folder + "/out" + "}")
 
     main_written.append(main[-1])
     main_written = "\n".join(main_written)
